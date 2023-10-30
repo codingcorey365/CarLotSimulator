5da662adac9fe82f92966df5a0d24db494816108
Corey Schroeder <codingcorey365@gmail.com>
2023-10-30 18:36:07 -0500

    correct branch before merge

--- CarLotSimulatorApp/CarLotSimulator/CarLot.cs
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -4,24 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CarLotSimulator // namespace
+namespace CarLotSimulator
 {
-    //Done: Create a CarLot class
-    //It should have at least one property: a List of cars
-    //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
-    //At the end iterate through the list printing each of car's Year, Make, and Model to the console
-
     public class CarLot // class
     {
-        public List<Car> CarsOnLot;// declaring a list
+        public CarLot() // constructor
+        {
         
-        public static int NumberOfCarsOnLot { get; set; } // ???? calls the variable?
-
-        public CarLot() //constructor
-        {            
-            CarsOnLot = new List<Car>(); // List made in the scope of constructor
         }
 
-        // look up custom getters! :D
+        public List<Car> listOfCars = new List<Car>();
+
     }
 }
--- CarLotSimulatorApp/CarLotSimulator/car.cs
+++ CarLotSimulatorApp/CarLotSimulator/car.cs
@@ -1,77 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-namespace CarLotSimulator //namespace
+namespace CarLotSimulator
 {
-
-    //Done: Create a seperate class file called Car
-    //Done: Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-    //Done: Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-    //The methods should take one string parameter: the respective noise property
-
-
-    public class Car //class
+    public class Car // class
     {
-
-        // ------------------------------------- constructors ------------------------------------------------------------
-
-        public Car()//default constructor
+        public Car() // default constructor
         {
-            CarLot.NumberOfCarsOnLot++;
+            
         }
 
-        public Car(string engineNoise, string honkNoise): this() //custom constructor
-        {
-            EngineNoise = engineNoise;
-            HonkNoise = honkNoise;// property and value
-        }
-
-        public Car(int year, string make, string model): this() //custom constructor
+        public Car(int year, string make, string model, string engineNoise, string honkNoise, bool isDrivable) // custom constructor
         {
             Year = year;
             Make = make;
-            Model = model;            
-        }
-
-        public Car(int year, string make, string model, string engineNoise, string honkNoise, bool isDriveable) : this(year, make, model) //custom constructor
-        {            
+            Model = model;
             EngineNoise = engineNoise;
             HonkNoise = honkNoise;
-            IsDriveable = isDriveable;
+            IsDrivable = isDrivable;
         }
 
-
-        // ----------------------------------------- Properties  ---------------------------------------
-
-        public int Year {  get; set; }
-        public string Make {  get; set; }
-        public string Model {  get; set; }
-        public string EngineNoise { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise {  get; set; }
         public string HonkNoise { get; set; }
-        public bool IsDriveable { get; set; }
+        public bool IsDrivable { get; set; }
 
-
-        //-------------------------------------------- Methods ----------------------------------------
-
-        public void MakeEngineNoise()
+        public void MakeEngineNoise(string engineNoise)
         {
-            
-            Console.WriteLine(EngineNoise); 
+            Console.WriteLine(engineNoise); 
         }
 
-        //-------------------------------------------- Methods ----------------------------------------
-
-        public void MakeHonkNoise()
+        public void MakeHonkNoise(string honkNoise)
         {
-            
-            Console.WriteLine(HonkNoise); 
+            Console.WriteLine(honkNoise); 
         }
-
-        //-------------------------------------------- Methods ----------------------------------------
-        public override string ToString()
-        {
-            return $"{Year} {Make} {Model}";
-        }
-
     }
+
 }
